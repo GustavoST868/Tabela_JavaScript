9cbfc0ad6d593b962b2cbc2047a374dec9ecfbe5
Gustavo Santos Teixeira <gustavosantosteixeira98@gmail.com>
2024-01-06 23:01:47 -0300

    .

--- Table in JavaScript/index.js
+++ Table in JavaScript/index.js
@@ -1,13 +1,15 @@
+//
 document.addEventListener('DOMContentLoaded', function () {
     
+    //stores data corresponding to tables
     const names = ['Produto A', 'Produto B', 'Produto C'];
     const prices = ['10.00', '20.50', '15.99'];
     const descriptions = ['Descrição do Produto A', 'Descrição do Produto B', 'Descrição do Produto C'];
 
-    
+    //
     const tableBody = document.querySelector('#productTable tbody');
 
-    
+    //
     for (let i = 0; i < names.length; i++) {
         const row = tableBody.insertRow();
         const cellName = row.insertCell(0);
